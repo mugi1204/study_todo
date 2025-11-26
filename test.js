3c3c45e2de1d3d3f7bd57db14aefef6dd9eb868d
mugi1204 <210929173+mugi1204@users.noreply.github.com>
2025-11-26 10:21:37 +0900

    リファクタリング

--- test.js
+++ test.js
@@ -2,18 +2,18 @@ const inputForm = document.getElementById("inputForm");
 const formBtn = document.getElementById("formBtn");
 const todoList = document.getElementById("todoList");
 
-formBtn.addEventListener('click',function(){
+formBtn.addEventListener('click', function() {
     const task = inputForm.value;
     const listItem = document.createElement('li'); 
     listItem.textContent = task; 
     todoList.appendChild(listItem);
-        const deleteBtn= document.createElement('button');
+        const deleteBtn = document.createElement('button');
         deleteBtn.textContent = '削除'; 
-        deleteBtn.addEventListener('click',function(){
+        deleteBtn.addEventListener('click', function() {
             listItem.remove();
         })
     listItem.appendChild(deleteBtn);
-    inputForm.value='';
+    inputForm.value = '';
 });
 
 
